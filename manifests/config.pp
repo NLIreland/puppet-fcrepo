b249eca6ef1ba1eefb1c41cebcbd65f61f55fa4b
ekilfeather <ekilfeather@gmail.com>
2017-10-12 11:42:01 +0100

    remove reference to repository.json

--- manifests/config.pp
+++ manifests/config.pp
@@ -30,28 +30,6 @@ class fcrepo::config {
     mode    => '0755',
     require => [ File[$::fcrepo::fcrepo_sandbox_home_real] ]
   }
-
-  # Put in place Fedora config repository.json
-  # It would be easier to use file for these, but file won't support https URLs until 
-  # Puppet 4.4
-  staging::file { 'repository.json':
-    target  => "${::fcrepo::fcrepo_configdir_real}/repository.json",
-    source  => $::fcrepo::fcrepo_repository_json_real,
-    require => File[$::fcrepo::fcrepo_configdir_real],
-  }->
-  file { "${::fcrepo::fcrepo_configdir_real}/repository.json":
-    ensure => present,
-    group  => $::fcrepo::group_real,
-    owner  => $::fcrepo::user_real,
-    mode   => '0644',
-  }->
-  exec { 'replace infinispan config path':
-    command     => "/bin/sed -i -e's|\\$.fcrepo.ispn.configuration:config.*infinispan.xml.|${::fcrepo::fcrepo_configdir_real}/infinispan.xml|' '${::fcrepo::fcrepo_configdir_real}/repository.json'",
-    path        => '/bin',
-    subscribe   => File["${::fcrepo::fcrepo_configdir_real}/repository.json"],
-    refreshonly => true,
-  }
-  
   # Put in place Fedora config jgroups-fcrepo-tcp.xml
   staging::file { 'jgroups-fcrepo-tcp.xml':
     target  => "${::fcrepo::fcrepo_configdir_real}/jgroups-fcrepo-tcp.xml",
